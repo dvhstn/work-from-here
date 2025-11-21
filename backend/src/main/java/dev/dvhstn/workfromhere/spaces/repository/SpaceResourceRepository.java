7334bb989ce1cab56531bdd63819fc7e9df3a0dd
David Houston <davidaehouston@gmail.com>
2025-11-21 19:25:49 +0000

    Adding Skeleton classes for Spaces

---
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/repository/SpaceResourceRepository.java
@@ -0,0 +1,4 @@
+package dev.dvhstn.workfromhere.spaces.repository;
+
+public interface SpaceResourceRepository {
+}
---
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/service/SpaceResourceService.java
@@ -0,0 +1,7 @@
+package dev.dvhstn.workfromhere.spaces.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpaceResourceService {
+}
