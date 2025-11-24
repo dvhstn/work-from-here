b529b2fc59efdb188ccb5b6ef5863e002a22740c
David Houston <davidaehouston@gmail.com>
2025-11-24 21:16:22 +0000

    Modifying model, fixing previous PR

--- backend/src/main/java/dev/dvhstn/workfromhere/spaces/model/SpaceResource.java
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/model/SpaceResource.java
@@ -10,4 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SpaceResource {
+    private Long id;
+    private String name;
+    private String description;
+    private SpaceTypeResource type;
+    private boolean wifiAvailable;
+    private String wifiPassword;
 }
--- backend/src/main/java/dev/dvhstn/workfromhere/spaces/model/SpaceTypeResource.java
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/model/SpaceTypeResource.java
@@ -1,4 +1,6 @@
 package dev.dvhstn.workfromhere.spaces.model;
 
 public enum SpaceTypeResource {
+    CAFE,
+    HOT_DESK
 }
