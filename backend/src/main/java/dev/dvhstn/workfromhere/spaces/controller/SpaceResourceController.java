b1c7159bbf5b1012f6fd7fe5c428164f7a525aea
David Houston <davidaehouston@gmail.com>
2025-12-03 19:29:30 +0000

    Skeleton Methods for Spaces Controller

--- backend/src/main/java/dev/dvhstn/workfromhere/spaces/controller/SpaceResourceController.java
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/controller/SpaceResourceController.java
@@ -1,7 +1,44 @@
 package dev.dvhstn.workfromhere.spaces.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import dev.dvhstn.workfromhere.spaces.model.SpaceResource;
+import dev.dvhstn.workfromhere.spaces.service.SpaceResourceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(value = "/api/v1/spaces")
 public class SpaceResourceController {
+
+    private SpaceResourceService spaceResourceService;
+
+    public SpaceResourceController(SpaceResourceService spaceResourceService) {
+        this.spaceResourceService = spaceResourceService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<SpaceResource>> getAllSpaces() {
+        return null;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<SpaceResource> getSpaceById(@PathVariable String id) {
+        return null;
+    }
+
+    @PostMapping()
+    public ResponseEntity<SpaceResource> createSpaceResource(@RequestBody SpaceResource spaceResource) {
+        return null;
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<SpaceResource> updateSpaceResource(@PathVariable String id, @RequestBody SpaceResource spaceResource) {
+        return null;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<SpaceResource> deleteSpaceResource(@PathVariable String id) {
+        return null;
+    }
 }
