d0acbf4afc4a9138027e4376fdd76b735eeeff23
David Houston <davidaehouston@gmail.com>
2026-01-13 17:08:27 +0000

    Updating SpaceType ENUM to habe Id

--- backend/src/main/java/dev/dvhstn/workfromhere/spaces/model/SpaceTypeResource.java
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/model/SpaceTypeResource.java
@@ -1,6 +1,19 @@
 package dev.dvhstn.workfromhere.spaces.model;
 
+import lombok.Getter;
+
+@Getter
 public enum SpaceTypeResource {
-    CAFE,
-    HOT_DESK
+    CAFE(1, "Cafe"),
+    HOT_DESK(2, "Hot Desk");
+
+    private final Integer id;
+    private final String name;
+
+    SpaceTypeResource(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
 }
