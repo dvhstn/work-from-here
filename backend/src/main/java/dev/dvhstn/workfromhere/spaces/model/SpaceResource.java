e10e091fcf1f57df856c232ad48364be69382db3
David Houston <davidaehouston@gmail.com>
2026-02-09 20:52:27 +0000

    Adding Java Persistence to POM, POJO validation

--- backend/src/main/java/dev/dvhstn/workfromhere/spaces/model/SpaceResource.java
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/model/SpaceResource.java
@@ -1,5 +1,14 @@
 package dev.dvhstn.workfromhere.spaces.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,11 +18,25 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "spaces")
 public class SpaceResource {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true,  nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private SpaceTypeResource type;
+
+    @Column(nullable = false)
     private boolean wifiAvailable;
+
     private String wifiPassword;
 }
