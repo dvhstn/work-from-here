7c724246860ac0f7576cbd38d8d40fafd735da42
David Houston <davidaehouston@gmail.com>
2026-02-23 09:47:16 +0000

    fix: fix imports

--- backend/src/main/java/dev/dvhstn/workfromhere/spaces/repository/SpaceResourceRepository.java
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/repository/SpaceResourceRepository.java
@@ -3,7 +3,5 @@ package dev.dvhstn.workfromhere.spaces.repository;
 import dev.dvhstn.workfromhere.spaces.model.SpaceResource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface SpaceResourceRepository extends JpaRepository<SpaceResource, Long> {
 }
