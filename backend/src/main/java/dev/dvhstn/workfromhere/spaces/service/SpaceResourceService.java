503038396ad431ee93795474cda47110b86d52d0
David Houston <davidaehouston@gmail.com>
2026-03-02 17:51:03 +0000

    fix: Making get all spaces pageable (#31)

--- backend/src/main/java/dev/dvhstn/workfromhere/spaces/service/SpaceResourceService.java
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/service/SpaceResourceService.java
@@ -8,10 +8,11 @@ import dev.dvhstn.workfromhere.spaces.mapper.SpaceResourceMapper;
 import dev.dvhstn.workfromhere.spaces.model.SpaceResource;
 import dev.dvhstn.workfromhere.spaces.model.SpaceTypeResource;
 import dev.dvhstn.workfromhere.spaces.repository.SpaceResourceRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -24,11 +25,9 @@ public class SpaceResourceService {
         this.spaceResourceRepository = spaceResourceRepository;
     }
 
-    public List<SpaceResponseDTO> getAllSpaces() {
-
-        return spaceResourceRepository.findAll().stream()
-                .map(spaceResourceMapper::toSpaceResponseDTO)
-                .toList();
+    public Page<SpaceResponseDTO> getAllSpaces(Pageable pageable) {
+        return spaceResourceRepository.findAll(pageable)
+                .map(spaceResourceMapper::toSpaceResponseDTO);
     }
 
     public SpaceResponseDTO getSpaceResourceById(Long id) {
