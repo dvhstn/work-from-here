4821098b1b59fe97ecc251e59e7f09f74c5fca5b
David Houston <davidaehouston@gmail.com>
2026-04-29 20:56:57 +0100

    consolidating flyway script changes (#59)

--- backend/src/main/resources/db/migration/V1__create_spaces_table.sql
+++ backend/src/main/resources/db/migration/V1__create_spaces_table.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS spaces
 (
-    id             BIGSERIAL    NOT NULL PRIMARY KEY,
-    name           VARCHAR(255) NOT NULL UNIQUE,
-    description    VARCHAR(255) NOT NULL,
-    type           VARCHAR(50)  NOT NULL,
-    wifi_available BOOLEAN      NOT NULL,
-    wifi_password  VARCHAR(255)
+    id            BIGSERIAL    NOT NULL PRIMARY KEY,
+    name          VARCHAR(100) NOT NULL UNIQUE,
+    description   VARCHAR(255) NOT NULL,
+    type          VARCHAR(50)  NOT NULL,
+    wifi_password VARCHAR(100)
 );
