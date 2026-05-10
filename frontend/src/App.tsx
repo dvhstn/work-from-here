c992e1ccf2e1ea7115b03d280340f2e298ce8c9f
David <davidaehouston@gmail.com>
2026-05-10 07:46:43 +0100

    Base commit for frontend code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <main className="min-h-screen bg-slate-100">
+      <div className="max-w-4xl mx-auto p-8">
+        <h1 className="text-4xl font-bold text-slate-800">
+          Work From Here
+        </h1>
+      </div>
+    </main>
+  )
+}
